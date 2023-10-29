cb9da7ec4c46eb633e237bda62f6e681475325db
Conrado <zafra.conrado@gmail.com>
2023-10-29 19:39:23 +0100

    feature(home): first draft

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  return (
+    <div className='tw-h-screen tw-w-screen tw-bg-amber-50 tw-pt-20 dark:tw-bg-stone-700'>
+      <div className='tw-h-full tw-w-full tw-bg-[url("/board-games.webp")] tw-bg-contain tw-bg-top tw-bg-repeat-space lg:tw-bg-no-repeat 2xl:tw-bg-cover'>
+        {' '}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
